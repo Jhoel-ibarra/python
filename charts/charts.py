012ec4814890add089128ff54b83fad70d41dd19
Jhoel-python <jhoel.juaquin7@gmail.com>
2022-11-13 17:37:13 -0400

    cambios del sabdo 12

--- charts/charts.py
+++ charts/charts.py
@@ -1,11 +1,23 @@
 import matplotlib.pyplot as plt 
 
-
+# para crear una grafica redondo (pie)
 def generate_pie_chart():
     labels = ["a", "b", "c"]
     values = [200, 43, 120]
 
     fig , ax = plt.subplots()
     ax.pie(values, labels = labels)
-    plt.savefig("pie.png")
+    plt.savefig("pie.png") #para crear una carpeta y guardar la grafica 
     plt.close()
+
+# para crear una grafica en barra lili (bar) 
+labels = ["a", "b", "c"]
+values = [200, 43, 120]
+
+def generate_bar_chart(labels , values):
+  fig, ax = plt.subplots()
+  ax.bar(labels, values)
+  plt.savefig("bar.png")
+  plt.close()
+
+ ## plt.show()  para q se detenga en ese punto 
