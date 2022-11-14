b19666862d6d6a9afc5c3b5dbbe52f59ae2554b9
Jhoel-python <jhoel.juaquin7@gmail.com>
2022-11-13 20:11:27 -0400

    leccion de fastApi y usando uvicorn

--- wed-serve/main.py
+++ wed-serve/main.py
@@ -1,4 +1,34 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/")
+def get_list():
+    return [1,2,4]
+
+@app.get("/contact")
+def get_list():
+    return {
+        "name" : "platzi"
+    }
+
+@app.get("/html", response_class=HTMLResponse)
+def get_list():
+    return """
+    <html>
+        <head>
+            <title>hola soy jhoel</title>
+        </head>
+        <body>
+            <h1>holanda</h1>
+        </body>
+    </html>
+    """
+
+
+
 def run():
     store.get_categories()
 
